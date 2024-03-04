ced923876e5b5dd5746f3c35008f5bf1361e44ae
Samuel Mboka <samuelmboka088@gmail.com>
2024-03-04 10:02:28 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Please Enter Your Name: ")
+age = input("Enter your age: ")
+location = input("Enter your location of residence: ")
+
+
+print("Good evening "+name+ ", you are " + age+ " years old. The ordered product will be shiped to your location  " + location )
